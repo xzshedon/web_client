01388be3e3e54a5e5f9358301659459310a33b01
Gui Shaoli <guishaoli@tp-link.com.cn>
2018-02-24 19:44:00 +0800

    修改浏览器配置方式，改为在用例中自定义浏览器、是否使用本地cookie、测试的url

--- framework/browser_engine.py
+++ framework/browser_engine.py
@@ -15,29 +15,44 @@ base_path = os.path.dirname(os.path.abspath('.'))
 
 class BrowserEngine(object):
     chrome_driver_path = base_path + '/tools/chromedriver.exe'
+
     # ie_driver_path = dir + '/tools/IEDriverServer.exe'
 
     def __init__(self, driver):
         self.driver = driver
 
     # 通过config.ini确定使用那个浏览器, 并返回driver对象
-    def open_browser(self, driver):
-        config = ConfigParser.ConfigParser()
-        # file_path = os.path.dirname(os.getcwd()) + '/config/config.ini'
-        file_path = base_path + '/config/config.ini'
-        config.read(file_path)
+    def open_browser(self, driver,conf):
+        '''
 
-        browser = config.get("browserType", "browserName")
+        :param driver:
+        :param conf: { "browserName":"Chrome","cookieData":"No","URL":"https://i.neffos.com/FOTA/to_imei"}
+        :return:
+        '''
+        # config = ConfigParser.ConfigParser()
+        # file_path = os.path.dirname(os.getcwd()) + '/config/config.ini'
+        # file_path = base_path + '/config/config.ini'
+        # config.read(file_path)
+
+        # browser = config.get("browserType", "browserName")
+        browser = conf["browserName"]
         logger.info("You had select %s browser." % browser)
-        url = config.get("testServer", "URL")
+        # url = config.get("testServer", "URL")
+        url = conf["URL"]
         logger.info("The test server url is: %s" % url)
+        # userdata = config.get("browserUserData", "userdata")
+        userdata = conf["cookieData"]
+        logger.info("The userdata is :%s" % userdata)
 
         if browser == "Firefox":
             driver = webdriver.Firefox()
             logger.info("Starting firefox browser.")
         elif browser == "Chrome":
             options = webdriver.ChromeOptions()
-            options.add_experimental_option("excludeSwitches",["ignore-certificate-errors"])
+            if userdata != "No":
+                options.add_argument(
+                    "--user-data-dir=" + userdata)
+            options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
             # options.add_argument("--test-type", ["--ignore-certificate-errors"])
             driver = webdriver.Chrome(self.chrome_driver_path, chrome_options=options)
             logger.info("Starting Chrome browser.")
